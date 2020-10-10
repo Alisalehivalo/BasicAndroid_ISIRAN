58af5efcc19192b6e95a98c9edd6a606fd9e4a76
salehi <akbarsalehi@gmail.com>
2020-10-10 17:02:57 +0330

    LifeCycle Added

--- app/src/main/java/ir/laitec/basicandroid_isiran/MainActivity.java
+++ app/src/main/java/ir/laitec/basicandroid_isiran/MainActivity.java
@@ -12,7 +12,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MyTag","in On Create!!!");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MyTag","in on Start!!!");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MyTag","in on Resume!!!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MyTag","in on Pause!!!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MyTag","in on Stop!!!");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MyTag","in on Restart!!!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MyTag","in on Destroy!!!");
     }
 }
